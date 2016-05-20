7ad63defe06a670e801b7c26e3d733e8e65fc272
javenkn <javenkn7@yahoo.com>
2016-05-19 20:08:37 -1000

    passed all tasks

--- context.js
+++ context.js
@@ -23,8 +23,8 @@ console.log( add(0,1) );
 console.log( add.call(voltron,2,3) );
 
 // #3
-console.log(  );
+console.log( add.apply(optimus, [20, 30]) );
 
 // #4
-
-console.log(  );
+var calculate = add.bind(bender);
+console.log( calculate("drinking", "beer") );
